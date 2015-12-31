96b95c8d4cc152351144f8bd1c8bd2c2b34bef1e
Nemurimasu Neiro <nemurimasu@gmail.com>
2015-12-31 05:57:13 +0000

    remove server rendering

--- src/server.js
+++ src/server.js
@@ -12,7 +12,6 @@ import path from 'path';
 import express from 'express';
 import React from 'react';
 import ReactDOM from 'react-dom/server';
-import Router from './routes';
 import Html from './components/Html';
 import assets from './assets';
 import { listenAddress, port } from './config';
@@ -31,18 +30,6 @@ server.get('*', async (req, res, next) => {
   try {
     let statusCode = 200;
     const data = { title: '', description: '', css: '', body: '', entry: assets.main.js };
-    const css = [];
-    const context = {
-      insertCss: styles => css.push(styles._getCss()),
-      onSetTitle: value => data.title = value,
-      onSetMeta: (key, value) => data[key] = value,
-      onPageNotFound: () => statusCode = 404,
-    };
-
-    await Router.dispatch({ path: req.path, query: req.query, context }, (state, component) => {
-      data.body = ReactDOM.renderToString(component);
-      data.css = css.join('');
-    });
 
     const html = ReactDOM.renderToStaticMarkup(<Html {...data} />);
     res.status(statusCode).send('<!doctype html>\n' + html);
